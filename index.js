7229f443368de3e474ab64c479feb7715fff724b
Sudip Adhikari <sadhikaris2016@gmail.com>
2021-12-29 01:11:42 -0600

    second day

---
+++ index.js
@@ -0,0 +1,119 @@
+// inliine comment 
+
+console.log(" hello world ")
+
+
+/* data types:
+undefined, null , boolean, striing, symbol, number, and object
+*/
+
+// TOPIC 
+//declare a variable
+
+var myName = " Sudip"
+myName = 8
+
+// var is for whole program
+// let is for the scope of where the deckleration is 
+// const can never be changed
+
+console.log ("myName");
+console.log(myName);
+
+var a;
+var b =2;
+console.log(a);
+console.log(b)
+
+a=7;
+b=a;
+
+console.log(a);
+console.log(b)
+
+var a =9;
+var b = 10;
+var c = "sudip" ;
+console.log("i am " + c) 
+
+// java script is case sensitive 
+// use camel case 
+
+
+// TOPIC
+//addition'
+
+var sum = 10 + 100;
+console.log (sum) ;
+
+// same goe4s for substriction
+// multiplication uses * 
+// division uses /
+
+// increment a number ( meaning  adding 1)
+// decrement a number ( substract 1) ( same process as increment )
+
+
+sum ++;
+sum = sum + 1; // ( does the same thing )
+ console.log(sum)
+ 
+ var product = sum * 2.7;
+ console.log(product)
+ 
+ //remainder operator used by %
+ // to find the remainder of 11 divided by 2
+ 
+ var remaninder = 11%2;
+ console.log(remaninder)  // got the answer 1 in the console
+ 
+ 
+ var a =3 
+ var b = 17
+ var c = 12
+ 
+ console .log ( a , b ,  c)
+ a = a + 1 // same thing in both line ( same for substraction, multiplication and division)
+ a += 1;
+ console.log(a)
+ 
+ 
+ // use a " " inside the string
+ var sudip = "sudipa"
+ console.log(sudip)
+ 
+ //quote in a string
+ var sudipp = " sud \"ip\"  man dallas ";
+ console.log(sudipp)
+ 
+ var mystr ="first line\n\t\\second line \n third line"
+ console.log(mystr)
+ 
+ //
+ var myName = " Sudip";
+ var myStr =" My name is " + myName ;
+ 
+ console.log(myStr)
+ //var myNameLength
+ console.log(myStr + " , " +  myStr.length)
+ 
+ 
+ 
+ 
+ // getting first and last letter of any name 
+ 
+var  LastLetterofmyName = myName[4];
+ console.log(LastLetterofmyName) 
+ 
+ // to find the last letter [firstName.length -1]
+ var lastLetterMyName = myName[myName.length - 1]
+ console.log(lastLetterMyName)
+ console.log(2==3)
+ console.log(2==2)
+ 
+ /*var num = [1,2,3,4,5,6,7,8,9,10]
+ for(var i =0 ; i < num.length; i ++){
+     console.log(i)
+     
+ }
+ */
